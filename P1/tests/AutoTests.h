dac6e0159645c0e77d9a9744fef3432020cb950f
xdx1354 <stanislaw1354@gmail.com>
2023-10-29 19:26:08 +0100

    AutoTests class added, minor Menu class fixes

--- P1/tests/AutoTests.h
+++ P1/tests/AutoTests.h
@@ -4,10 +4,20 @@
 
 #ifndef P1_AUTOTESTS_H
 #define P1_AUTOTESTS_H
-
+#define NUMBER_OF_TESTS 20
 
 class AutoTests {
 
+public:
+    void generateAllData();        //generating data for autotests. Set for specific sizes of graphs
+
+    // auto test based on generated data. Include time measurement. Calculates avg time of x repetitions for each quantity
+    void autoTestBF();
+    void autoTestBB();
+    void autoTestDP();
+
+
+    void generateData(int size);
 };
 
 
