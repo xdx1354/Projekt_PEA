b75bc61ecc46ab4f65079dfa775e013cf38720cf
xdx1354 <stanislaw1354@gmail.com>
2023-11-18 18:00:33 +0100

    further development of TS

--- P2/algorithms/TS.cpp
+++ P2/algorithms/TS.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "TS.h"
+#include "random"
 using namespace std;
 
 TS::TS(Graph graph){
@@ -14,9 +15,17 @@ TS::TS(Graph graph){
 
 
 void TS::apply(int maxIterations){
-    // setting initial result
+
     currentPath = generateFirstPath();
     bestPath = currentPath;
+
+    // initializing the tabu list
+    tabulist = new int*[maxIterations];
+    for (int i = 0; i < maxIterations; ++i) {
+        tabulist[i] = new int[numOfCities];
+    }
+
+    //TODO: initialize the bestPath, neighbourPath, currentPath
 
     /// loop trying to calculate best solution
     for(int i = 0; i < maxIterations; i++){
@@ -26,10 +35,57 @@ void TS::apply(int maxIterations){
 
 
 void TS::epoch() {
-    // calcualate new path
-    currentPath = generateNextPath();
-    if(calculatePathCost(currentPath) < bestPathCost){
+    // calculate new path
+    generateNeighbourPath();
+    neighbourPathCost = calculatePathCost(neighbourPath);
+
+    if( neighbourPathCost < currentPathCost){
+        currentPath = neighbourPath;
+        currentPathCost = neighbourPathCost;
+
+        // keeping track of the best ever found result
+        if(currentPathCost < bestPathCost){
+            bestPath = currentPath;
+            bestPathCost = currentPathCost;
+        }
+
+
+        //?? any other restrains should be added despite adding this path to tabu just after generating it???
 
     }
+    else {
+        // randomly chosen chance generated to pick neighbour solution even if it worse than currently held one
+        // in purpose to leave the local minimum
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(0.0, 1.0);
+        double random_number = dis(gen);
+
+        if (random_number < 1.0 - 1.0 / tabulistLength) {
+            currentPath = neighbourPath;
+            currentPathCost = neighbourPathCost;
+        }
+    }
+}
+/**
+ * generates neighbour path for path stored in currentPath
+ * @return generated path as integer pointer
+ */
+void TS::generateNeighbourPath() {
+    // should add the generated result to tabu list
+    int pos1 = rand() % (numOfCities - 1) + 1;
+    int pos2 = rand() % (numOfCities - 1) + 1;
+
+    // assert that pos1 and pos2 are not the same
+    // excludes possibility of empty iteration when getting no change in path
+    while (pos1 == pos2){
+        pos2 = rand() % (numOfCities - 1) + 1;
+    }
+//    int* neighbor_solution = new int[numOfCities];
+    memcpy(neighbourPath, currentPath, numOfCities * sizeof(int));      // copying the data to new array
+    swap(neighbourPath[pos1], neighbourPath[pos2]);                     // swapping two cities
+    neighbourPathCost = calculatePathCost(neighbourPath);               // updating the cost
+
+    ///TODO: check tabu, if its already in tabu list -> generate again and so on
 }
 
--- P2/algorithms/TS.h
+++ P2/algorithms/TS.h
@@ -13,11 +13,11 @@ class TS {
 public:
     TS(Graph graph);
     void apply(int maxIterations);               // running the algorithm, preparing var and objects
-    int* generateFirstPath();   // generates initial path with selected method
-    int* generateNextPath();    // generates path using certain method (swaps etc. TODO: decide on a method)
+    int* generateFirstPath();   // generates initial path with selected method  // TODO: can be void if data will be passed directly to field of class
+    void generateNeighbourPath();    // generates path using certain method (swaps etc. TODO: decide on a method)
     void epoch();               // loop method, loop for set times and tries to calculate the most optimal res
-    int calculatePathCost(int currentPath[]);   // for a given path calculates it cost
-    bool findInTabu(int currentPath[]);          // checks if given path is present in tabu list
+    int calculatePathCost(int path[]);   // for a given path calculates it cost
+    bool findInTabu(int path[]);          // checks if given path is present in tabu list
 
 
     ///COMMENTS
@@ -31,8 +31,13 @@ public:
     int *bestPath;              // stores the best already found path, should be initialized with all -1?
     int bestPathCost = INT_MAX;
     int *currentPath;           // stores currently calculated path
+    int currentPathCost = INT_MAX;
+    int *neighbourPath;
+    int neighbourPathCost = INT_MAX;
     int endCondition;
     int **matrix;               // extracting the matrix from graph class to have more convenient access
+    int **tabulist;
+    int tabulistLength;
 
 };
 
